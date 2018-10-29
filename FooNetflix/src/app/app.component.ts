07fd8f3bc4dbbb49ea234370b1baa9bd4b437e9a
LUIZ FERNANDO DE SANTANA <luiz.santana@corp.nova>
2018-10-29 16:59:41 -0300

    correção nos itens e colunas que estvam errados

--- FooNetflix/src/app/app.component.ts
+++ FooNetflix/src/app/app.component.ts
@@ -3,12 +3,17 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+// import { LoginPage } from './login/login.page';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
+  //templateUrl: '<ion-nav [root]="rootPage"></ion-nav>'
 })
 export class AppComponent {
+
+  // public rootPage:   any = LoginPage;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
